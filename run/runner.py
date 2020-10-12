7700fccbcb57807f57067522eaedfff598eefafb
2636419668@qq.com <2636419668@qq.com>
2020-10-12 14:08:36 +0800

    封装执行器

---
+++ run/runner.py
@@ -0,0 +1,21 @@
+# @Time : 2020/10/12 10:37
+# @Author : 帅帅
+# @Email : 2636419668@qq.com
+# @File : runner.py
+# @Project : ECShop
+import unittest
+import time
+from conf.config import Case_path,Report_path
+from BeautifulReport import BeautifulReport
+
+"""执行所有用例"""
+
+# 创建测试集
+suite = unittest.defaultTestLoader.discover(Case_path,"test_*.py")
+# 创建执行器
+runner = BeautifulReport(suite)
+# 执行
+filename = "{}{}.html".format("ECShop-report-",time.strftime("%Y%m%d%H%M"))
+runner.report(description="ECShop测试报告",
+              filename=filename,
+              report_dir=Report_path)
